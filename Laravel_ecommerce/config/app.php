3045e7ebb4390582730b5a28509c3a4a7158385d
Avishar37 <avisharpetwal37@gmail.com>
2025-11-04 14:09:31 +0530

    Commit

--- Laravel_ecommerce/config/app.php
+++ Laravel_ecommerce/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://charolette-uncontemptible-uncriticizably.ngrok-free.dev'),
 
     /*
     |--------------------------------------------------------------------------
